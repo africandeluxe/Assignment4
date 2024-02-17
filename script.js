a5a8a3ef5f4b4276aab2a2cf54c4d3ddfea29a6b
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-02-17 19:44:37 +0100

    Made some checks and adjustments for getting the data and to see if the data is displayed

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-document.getElementById('fetchButton').addEventListener("click", fetchData);
+document.getElementById('fetchButton').addEventListener('click', fetchData);
 
 function fetchData () {
-  const apiURL = '';
+  const apiURL = 'https://www.boredapi.com/api/activity';
 
   fetch(apiURL).then(response => {
     if (!response.ok) {
-      throw new Error('Network response was not ok: ${response.statusText}');
+      throw new Error(`Network response was not ok: ${response.statusText}`);
     }
     return response.json();
   }).then(data => {
     displayData(data);
   })
-  .catch((error) => {
+  .catch(error => {
     console.error('Error fetching data:', error);
   });
-  };
+  }
+
 
 function displayData(data) {
   const container = document.getElementById('container');
@@ -22,10 +23,8 @@ function displayData(data) {
   container.innerHTML = '';
 
   const titleElement = document.createElement('h2');
-
-  const bodyElement = document.createElement('p');
-  bodyElement.textContent = data.body;
-
+  
+  titleElement.textContent = data.activity;
+  
   container.appendChild(titleElement);
-  container.appendChild(bodyElement);
 }
